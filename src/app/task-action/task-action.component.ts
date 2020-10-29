452a21c3937cee78a8249d5f7203b80564d07e25
Pavel Shilkin <shilkin32731@yandex.ru>
2020-10-29 20:16:36 +0300

    add method in services - addTask + add component with controls for this method

---
+++ src/app/task-action/task-action.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Task, TaskService} from '../services/task-service';
+
+@Component({
+  selector: 'app-task-action',
+  templateUrl: './task-action.component.html',
+  styleUrls: ['./task-action.component.scss']
+})
+export class TaskActionComponent implements OnInit {
+  title: string = ''
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+  }
+
+  addTask(){
+    const task: Task = {
+      id: 0,
+      title: this.title,
+      completed: false,
+      date: new Date()
+    };
+    this.taskService.addTask(task);
+    this.title = ''
+  }
+}
